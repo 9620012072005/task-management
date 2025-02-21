86b44cf26b28e11197aa4a61e5eff569f80ced0b
9620012072005 <chellammal786@gmail.com>
2025-02-21 10:25:55 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true, // Ensure proper routing for React Router in development
+  },
+  build: {
+    outDir: 'dist', // Ensure Vercel knows where the build files are
+  }
 })
