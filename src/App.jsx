614b112774eeb842834ab9a067e91a741e4419b7
9620012072005 <chellammal786@gmail.com>
2025-02-21 10:11:17 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,52 +3,32 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
-
 // Import components
 import Menu from "./Menu";
 import ProjectList from "./Projects";
 import CompanyList from "./Company";
-import Pivot from "./Pivot"; 
+import TaskTable from "./Pivot"; // Make sure this file exists
 import TaskGraph from "./TaskGraph";
 import Kanban from "./Kanban";
-
-// import Useravatarupload from "./users/Useravatarupload";
 import AddUserForm from "./Users";
-
 
 const App = () => {
   return (
-   
-    <DndProvider backend={HTML5Backend}>
-      <Router>
+    <Router>
+      <DndProvider backend={HTML5Backend}>
         <div className="app-container">
-          
-          {/* Define your routes */}
           <Routes>
-            {/* Route for Company List - default landing page */}
             <Route path="/" element={<CompanyList />} />
-
-            {/* Route for Menu, with company ID and project ID as parameters */}
             <Route path="/menu/:id/:projectId" element={<Menu />} />
-
-            {/* Route for Projects, with company ID as a parameter */}
             <Route path="/projects/:id" element={<ProjectList />} />
-
-            {/* Route for Gantt chart */}
-           
-            
-
-            {/* Optional: Routes for other pages */}
-            <Route path="/pivot" element={<Pivot />} />
+            <Route path="/pivot" element={<TaskTable />} />
             <Route path="/graph" element={<TaskGraph />} />
             <Route path="/kanban" element={<Kanban />} />
             <Route path="/users" element={<AddUserForm />} />
-         
           </Routes>
         </div>
-      </Router>
-    </DndProvider>
-    
+      </DndProvider>
+    </Router>
   );
 };
 
